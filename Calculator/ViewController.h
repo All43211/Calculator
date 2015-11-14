b541acf6108d96517935a34e30ca36a9a344aeee
All43211 <orrin@altsteelemac.roc.mn.charter.com>
2015-11-14 00:24:04 -0600

    more changes... not sure why the buttons aren't working at the moment.

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -9,7 +9,31 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *calculatorTextBox;
 
+//Action Buttons
+@property (weak, nonatomic) IBOutlet UIButton *acButton;
+@property (weak, nonatomic) IBOutlet UIButton *signButton;
+@property (weak, nonatomic) IBOutlet UIButton *percentButton;
+@property (weak, nonatomic) IBOutlet UIButton *divideButton;
+@property (weak, nonatomic) IBOutlet UIButton *multiplyButton;
+@property (weak, nonatomic) IBOutlet UIButton *plusButton;
+@property (weak, nonatomic) IBOutlet UIButton *decimalButton;
+@property (weak, nonatomic) IBOutlet UIButton *equalButton;
+
+//Number Buttons
+@property (weak, nonatomic) IBOutlet UIButton *oneButton;
+@property (weak, nonatomic) IBOutlet UIButton *twoButton;
+@property (weak, nonatomic) IBOutlet UIButton *threeButton;
+@property (weak, nonatomic) IBOutlet UIButton *fourButton;
+@property (weak, nonatomic) IBOutlet UIButton *fiveButton;
+@property (weak, nonatomic) IBOutlet UIButton *sixButton;
+@property (weak, nonatomic) IBOutlet UIButton *sevenButton;
+@property (weak, nonatomic) IBOutlet UIButton *eightButton;
+@property (weak, nonatomic) IBOutlet UIButton *nineButton;
+@property (weak, nonatomic) IBOutlet UIButton *zeroButton;
+
+- (IBAction)addNumber:(id)sender;
 
 @end
 
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -20,6 +20,7 @@
     // Code added
     // More Changes
     //TGIF2
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,4 +28,29 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)addNumber:(id)sender {
+    NSString *localString = self.calculatorTextBox.text;
+    self.calculatorTextBox.text = @"90";
+//    if(sender == self.oneButton){
+//        self.calculatorTextBox.text = [NSString stringWithFormat:@"%@%s",localString,"1"];
+//    }else if (sender == self.twoButton){
+//        self.calculatorTextBox.text = [NSString stringWithFormat:@"%@%s",localString,"2"];
+//    }else if (sender == self.threeButton){
+//        self.calculatorTextBox.text = [NSString stringWithFormat:@"%@%s",localString,"3"];
+//    }else if (sender == self.fourButton){
+//        self.calculatorTextBox.text = [NSString stringWithFormat:@"%@%s",localString,"4"];
+//    }else if (sender == self.fiveButton){
+//        self.calculatorTextBox.text = [NSString stringWithFormat:@"%@%s",localString,"5"];
+//    }else if (sender == self.sixButton){
+//        self.calculatorTextBox.text = [NSString stringWithFormat:@"%@%s",localString,"6"];
+//    }else if (sender == self.sevenButton){
+//        self.calculatorTextBox.text = [NSString stringWithFormat:@"%@%s",localString,"7"];
+//    }else if (sender == self.eightButton){
+//        self.calculatorTextBox.text = [NSString stringWithFormat:@"%@%s",localString,"8"];
+//    }else if (sender == self.nineButton){
+//        self.calculatorTextBox.text = [NSString stringWithFormat:@"%@%s",localString,"9"];
+//    }else if (sender == self.zeroButton){
+//        self.calculatorTextBox.text = [NSString stringWithFormat:@"%@%s",localString,"0"];
+//    }
+}
 @end
