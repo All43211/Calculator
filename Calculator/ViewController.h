1ba35664a04996f946017724e242e9b8ec111f7c
All43211 <orrin@altsteelemac.roc.mn.charter.com>
2015-11-13 20:25:16 -0600

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Amy Steele on 11/13/15.
+//  Copyright © 2015 MSB SD352. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Amy Steele on 11/13/15.
+//  Copyright © 2015 MSB SD352. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
